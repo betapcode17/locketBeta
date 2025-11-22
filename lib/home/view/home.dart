2cdfcc26d00a1f865beb7352e369710ab88e80cd
Nguyen Ngoc <knkidngoc@gmail.com>
2025-11-23 00:16:13 +0700

    feat: add friend recommendation

--- lib/home/view/home.dart
+++ lib/home/view/home.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:locket_beta/camera/cubit/camera_state.dart';
 import 'package:locket_beta/friends/view/friends_screen.dart';
+import 'package:locket_beta/friends/cubit/friend_cubit.dart';
 import 'package:locket_beta/messenger/chat/chat.dart';
 import 'package:locket_beta/profile/profile.dart';
 import 'package:locket_beta/photo/cubit/photo_cubit.dart';
@@ -115,9 +116,26 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   GestureDetector(
                     onTap: () {
+                      // Navigator.of(context).push(
+                      //   MaterialPageRoute(
+                      //       builder: (_) => const FriendsScreen()),
+                      // );
+                      // Navigator.of(context).push(
+                      //   MaterialPageRoute(
+                      //     builder: (_) => BlocProvider.value(
+                      //       value: context.read<FriendCubit>(),
+                      //       child: FriendsScreen(),
+                      //     ),
+                      //   ),
+                      // );
+
                       Navigator.of(context).push(
                         MaterialPageRoute(
-                            builder: (_) => const FriendsScreen()),
+                          builder: (_) => BlocProvider(
+                            create: (_) => FriendCubit(),
+                            child: FriendsScreen(),
+                          ),
+                        ),
                       );
                     },
                     child: Container(
